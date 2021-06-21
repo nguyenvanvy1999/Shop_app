a5d698d0a9ddb9d04ab69cf8aa37522be0a9d38b
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-06-21 09:02:44 +0700

    #16927 [be] fix tool

--- backend/src/core/base/tools/file.tool.ts
+++ backend/src/core/base/tools/file.tool.ts
@@ -5,4 +5,7 @@ export const removeFilesByPaths = (paths: string[] | string): void => {
 	Array.isArray(paths) ? paths.forEach((path) => fs.unlinkSync(path)) : fs.unlinkSync(paths);
 };
 
-export const removeFilesError = (files: any): void => files.forEach((file) => fs.unlinkSync(`${config.rootPath}`));
+export const removeFilesError = (files: any): void =>
+	Array.isArray(files)
+		? files.forEach((file) => fs.unlinkSync(`${config.rootPath}/${file.path}`))
+		: fs.unlinkSync(`${config.rootPath}/${files.path}`);
