3854c056f90eb304728952d958c263166a44ea40
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-06-23 13:55:59 +0700

    #16982 [be] fix product validator

--- backend/src/core/product/validators/create.validator.ts
+++ backend/src/core/product/validators/create.validator.ts
@@ -1,13 +1,15 @@
 import { celebrate, Joi, Segments } from 'celebrate';
 
 export const ProductCreateVAL = celebrate({
-	[Segments.BODY]: Joi.object().keys({
-		title: Joi.string().required(),
-		ID: Joi.string().required(),
-		category: Joi.string().required(),
-		price: Joi.number().min(0).required(),
-		description: Joi.string().required(),
-		content: Joi.string().required(),
-		image: Joi.any(),
-	}),
+	[Segments.BODY]: Joi.object()
+		.keys({
+			title: Joi.string().required(),
+			ID: Joi.string().required(),
+			category: Joi.string().required(),
+			price: Joi.number().min(0).required(),
+			description: Joi.string().required(),
+			content: Joi.string().required(),
+			slide: Joi.array().items(Joi.any()),
+		})
+		.unknown(true),
 });
